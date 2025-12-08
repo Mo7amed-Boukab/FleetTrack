9bf13b805c32a0dbe7f48210995b4a7fd2b04d60
mohamed <mohamedboukab202@gmail.com>
2025-12-08 16:57:55 +0100

    FLT-7 Add error handling and not found middleware

---
+++ backend/src/middlewares/errorHandlerMiddleware.js
@@ -0,0 +1,11 @@
+const errorHandler = (err, req, res, next) => {
+ console.error(`${err.message} - ${req.method} ${req.originalUrl}`, { stack: err.stack });
+
+  const statusCode = err.statusCode || err.status || 500;
+  res.status(statusCode).json({
+    statusCode,
+    message: err.message || "Internal Server Error",
+    ...(err.errors && { errors: err.errors }),
+    ...(process.env.NODE_ENV !== "production" && { stack: err.stack })
+  });
+}
